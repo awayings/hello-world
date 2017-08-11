91ef1bd5e9e43fe0d1761f23a79977f9ddaa9949
zhangj <zhangj@gegejia.com>
2017-08-11 16:07:47 +0800

    fix.

--- common-utils/src/main/java/com/zhangj/research/utils/PerformanceUtil.java
+++ common-utils/src/main/java/com/zhangj/research/utils/PerformanceUtil.java
@@ -1,12 +1,5 @@
 package com.zhangj.research.utils;
 
-import com.sun.tools.javac.util.ArrayUtils;
-
-import javax.swing.*;
-import java.util.Collections;
-import java.util.Random;
-import java.util.function.Consumer;
-
 /**
  * Created by zhangjing on 2017/8/11.
  */
@@ -17,23 +10,23 @@ public class PerformanceUtil
         long st = System.nanoTime();
         runnable.run();
         long elapsed = System.nanoTime() - st;
-        System.out.println( prefix + "Elapsed:" + elapsed + " ns, " + ((double)elapsed/1000 /1000) + " ms");
+        System.out.println(prefix + "Elapsed:" + elapsed + " ns, " + ((double)elapsed / 1000 / 1000) + " ms");
         return elapsed;
     }
-
+    
     public static long elapsed(Runnable runnable)
     {
         return elapsed("", runnable);
     }
-
+    
     public static void main(String[] args)
     {
-        elapsed(()->{
+        elapsed(() -> {
             for (Integer i = 0; i < Integer.valueOf(1000); i++)
             {
                 Thread a = new Thread();
             }
         });
-
+        
     }
 }
